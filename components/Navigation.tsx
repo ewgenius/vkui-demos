e60e4d50fb0990d85af1f586e728959b3a63d18f
Evgenii Khramkov <ewgeniux@gmail.com>
2020-12-07 11:57:14 +0300

    upd demo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,16 +1,35 @@
 import { Icon24AddSquareOutline } from "@vkontakte/icons";
-import { CellButton, Group, Header } from "@vkontakte/vkui";
-import React from "react";
+import {
+  CellButton,
+  Group,
+  Header,
+  ModalCard,
+  ModalRoot,
+  View,
+} from "@vkontakte/vkui";
+import React, { useState } from "react";
 
 export const Navigation = () => {
+  const [modal, setModal] = useState(null);
   return (
-    <Group header={<Header>View without panels</Header>}>
-      <CellButton after={<Icon24AddSquareOutline />}>item 1</CellButton>
-      <CellButton>item 2</CellButton>
-      <CellButton>item 3</CellButton>
-      <CellButton>item 4</CellButton>
-      <CellButton>item 5</CellButton>
-      <CellButton>item 6</CellButton>
-    </Group>
+    <View
+      modal={
+        <ModalRoot activeModal={modal} onClose={() => setModal(null)}>
+          <ModalCard id="1" onClose={() => setModal(null)}>
+            test
+          </ModalCard>
+        </ModalRoot>
+      }
+    >
+      <Group header={<Header>View without panels</Header>}>
+        <CellButton after={<Icon24AddSquareOutline />}>item 1</CellButton>
+        <CellButton>item 2</CellButton>
+        <CellButton>item 3</CellButton>
+        <CellButton>item 4</CellButton>
+        <CellButton>item 5</CellButton>
+        <CellButton>item 6</CellButton>
+        <CellButton onClick={() => setModal("1")}>Open modal</CellButton>
+      </Group>
+    </View>
   );
 };
